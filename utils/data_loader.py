6dc3c4f70c158a00a26ece886fe5ed85a0119c4f
joonsang65 <junsang65@ajou.ac.kr>
2025-03-13 15:15:12 +0900

    add validation loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,15 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from data.dataset import *
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import matplotlib.pyplot as plt
 
-def make_loader(batch_size=64):
+# 난수 고정은 안 함 (난수 설정에 따라 데이터셋이 다르기 때문에 결과가 달라질 수 있음)
+def make_loader(batch_size=64, val_ratio=0.2):
     train_dataset =load_train_dataset()
     test_dataset = load_test_dataset()
 
+    # 데이터셋을 훈련용과 검증용으로 나누기
+    train_size = int(len(train_dataset) * (1 - val_ratio))
+    val_size = len(train_dataset) - train_size
+
+    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
     test_loader  = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
 
 
 # 생성한 데이터 로더 시각화 함수
@@ -26,3 +38,8 @@ def visualize_train_loader(train_loader):
         axes[i].axis('off')  # 축 제거
 
     plt.show()
+
+train_loader, _, _ = make_loader()
+
+
+visualize_train_loader(train_loader)
