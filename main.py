75b7aa7fe06f1b6e347dd43fedfbe9fd67bfecdb
배동우 <projectares777@gmail.com>
2025-03-13 18:51:19 +0900

    featureScratch

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ model = load_model(model_type)
 
 # 선택한 모델 학습하고 시각화하기.
 run = run_model(model_type, train_loader, val_loader, test_loader, lr=0.001, epochs=10, path='best_model.pth')
-visualize_predictions(model_type, test_loader, num_samples=5, path='best_model.pth'):
+visualize_predictions(model_type, test_loader, num_samples=5, path='best_model.pth')
