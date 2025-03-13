73ce43a5622568bc255d6795ab270a60b58c5d92
kyakyak <udosjdjdjdj@gmail.com>
2025-03-13 16:12:56 +0900

    modularization

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import os
+import torch
+# Scratch Class import
+# VGGNet Class import
+from models.ResNet import MNISTResNet
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODELS_DIR = os.path.join(BASE_DIR, 'models')
+
+model_class = {
+    'Scratch' : , # Scratch Class
+    'VGGNet' : , # VGGNet Class
+    'ResNet' : MNISTResNet()
+}
+
+model_dir = {
+    'Scratch' : 'Scratch_models', # Scratch Class
+    'VGGNet' : 'VGGNet_models', # VGGNet Class
+    'ResNet' : 'ResNet_models'
+}
+
+# 모델 함수
+def load_model(model_type, best=False, path=''):
+    model = model_class[model_type].to(DEVICE)
+    if best:
+        path = os.path.join(MODELS_DIR, model_dir[model_type], path)
+        model.load_state_dict(torch.load(path))
+    return model
